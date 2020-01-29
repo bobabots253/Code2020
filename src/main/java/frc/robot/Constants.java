a73355db323ef1a1ee1314c6f36ba21564b3730b
SabienNguyen <sabiennguyen@gmail.com>
2020-01-28 18:13:14 -0800

    remove unused deadband constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,7 +11,6 @@ public class Constants {
     public static class DriverConstants {
         // copied from 2019
         public static final double kJoystickDeadband = 0.07; // How much of joystick is "dead" zone [0,1]
-        public static final double kTriggerDeadband = 0.05;  // How much of trigger  is "dead" zone [0,1]
         
         public static final double kTurnSens = 1; // Maximum normal turning rate (in percent of max) to allow robot to turn to [0,1]
         public static final double kDriveSens = 1; // Overall speed setting (turn down for demos) [0,1]
