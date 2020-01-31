f066af35db1a5b49f4212fd8fc01f9259ba5960d
Sabien Nguyen <21nguyens54@mymhs.org>
2020-01-30 20:07:47 -0800

    Ok neotest should work lmao

---
+++ src/main/java/frc/robot/Command/Shoot.java
@@ -0,0 +1,43 @@
+
+package frc.robot.Command;
+
+import java.util.Set;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.RobotContainer;
+import frc.robot.Subsystem.*;
+
+public class Shoot implements Command {
+    
+    Subsystem[] requirements = { RobotContainer.shooter };
+    Shooter shooter = RobotContainer.shooter;
+    
+    public Shoot() {
+        
+    }
+    
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        double speed = RobotContainer.getJoystickYValue();
+        RobotContainer.shooter.shoot(speed);
+    }
+    
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        shooter.stopMotors();
+    }
+    
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    
+    @Override
+    public Set<Subsystem> getRequirements() {
+        return Set.of(requirements);
+    }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,8 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.DriverConstants;
-
+import frc.robot.Subsystem.*;
+import frc.robot.Command.*;
 
 public class RobotContainer {
     public static Shooter shooter;
