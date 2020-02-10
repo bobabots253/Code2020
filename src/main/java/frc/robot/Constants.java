885c26075fab7cc42221eef57f82b18364146063
SebbyLaw <44045823+SebbyLaw@users.noreply.github.com>
2020-02-09 22:24:21 -0800

    add example song

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -112,6 +112,7 @@ public class Constants {
                 All songs should have a file extension .chrp
                 All songs in this array should be just the filename without the extension (case sensitive)
                  */
+                "example"
         
         };
         
