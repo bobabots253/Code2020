dfebc70988e0bad402d08c850216cf906217497b
Max Kerns <60684506+Sakend@users.noreply.github.com>
2020-03-09 17:24:32 -0700

    it works now and some blink code

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -3,17 +3,18 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.CANifier;
 import com.ctre.phoenix.CANifier.LEDChannel;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants.LEDConstants;
  
 public class LED implements Subsystem{
     
-    CANifier canifier = new CANifier(0);
+    CANifier canifier = new CANifier(15);
     private static LED instance;
-    public static LEDConstants.State state;
+    public static LEDConstants.State state = LEDConstants.State.NULL;
     public static Color color;
-
+    public static double startTime = 0.0;
     private LED(){
         register();
     }  
@@ -22,7 +23,7 @@ public class LED implements Subsystem{
     public void periodic(){
         switch(state){
             case SHOOTING: { setColor(Color.kBlack, 0.0); break; }
-            case INTAKE: { setColor(Color.kBlue, 0.0); break; }
+            case INTAKE: { setColor(Color.kBlue, 0.2); break; }
             case ENABLED: { setColor(Color.kDarkTurquoise, 0.0); break; } 
             case DISABLED: { setColor(Color.kGreen, 0.0); break; }
             case ERROR: { setColor(Color.kRed, 0.0); break; }
@@ -35,9 +36,25 @@ public class LED implements Subsystem{
         return instance;
     }
     public void setRGB(double red, double green, double blue, double blinkTiming){
-       canifier.setLEDOutput(red, LEDChannel.LEDChannelA);
-       canifier.setLEDOutput(green, LEDChannel.LEDChannelB);
-       canifier.setLEDOutput(blue, LEDChannel.LEDChannelC);
+        if(blinkTiming == 0.0){
+            canifier.setLEDOutput(red, LEDChannel.LEDChannelA);
+            canifier.setLEDOutput(green, LEDChannel.LEDChannelB);
+            canifier.setLEDOutput(blue, LEDChannel.LEDChannelC);
+        }
+        else{
+        if(startTime == 0.0)   {startTime = Timer.getFPGATimestamp();}
+        if((Timer.getFPGATimestamp() - startTime ) < blinkTiming){
+            canifier.setLEDOutput(red, LEDChannel.LEDChannelA);
+            canifier.setLEDOutput(green, LEDChannel.LEDChannelB);
+            canifier.setLEDOutput(blue, LEDChannel.LEDChannelC);
+        }
+        else {
+            canifier.setLEDOutput(0.0, LEDChannel.LEDChannelA);
+            canifier.setLEDOutput(0.0, LEDChannel.LEDChannelB);
+            canifier.setLEDOutput(0.0, LEDChannel.LEDChannelC);
+            startTime = Timer.getFPGATimestamp();
+        }
+    }
     }
 
     public void setColor(Color color, double blinkTiming){
