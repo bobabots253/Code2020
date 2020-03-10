8889c48e8c06af1dade4ce8fb7b4520f0e853256
SebbyLaw <44045823+SebbyLaw@users.noreply.github.com>
2020-03-09 18:48:54 -0700

    add servo code
    Signed-off-by: SebbyLaw <44045823+SebbyLaw@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants;
 import frc.robot.Util;
@@ -11,11 +11,25 @@ public class Climber implements Subsystem {
     private static Climber instance;
     public static TalonSRX
          leftMotor = Util.createTalonSRX(Constants.ClimberConstants.leftMotorID, true),
-         rightMotor = Util.createTalonSRX(Constants.ClimberConstants.rightMotorID, true); 
+         rightMotor = Util.createTalonSRX(Constants.ClimberConstants.rightMotorID, true);
+    
+    private static Servo leftServo, rightServo;
 
     private Climber(){
         leftMotor.setInverted(false);
         rightMotor.setInverted(false);
+        leftServo = new Servo(Constants.ClimberConstants.leftServoID);
+        rightServo = new Servo(Constants.ClimberConstants.rightServoID);
+    }
+    
+    /**
+     * Set the servos' position.
+     * Servo values range from 0.0 to 1.0 corresponding to the range of full left to full right.
+     * @param value Position from 0.0 to 1.0.
+     */
+    public void setServos(double value) {
+        leftServo.set(value);
+        rightServo.set(value);
     }
 
     public void climbUnity(double speed){
